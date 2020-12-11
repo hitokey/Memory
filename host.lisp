20af8b083bff2d2a5527d73a79999322e2716242
hitokey <pedro_like07@hotmail.com>
2020-12-11 14:12:31 -0300

    interligado front-end and back-end

--- host.lisp
+++ host.lisp
@@ -26,16 +26,57 @@
   '(((:ID . 1)
      (:ITEM (:CLASS-NAME . "d-block w-100")
       (:NOME . "Slider1") (:WIDTH . "100px")
-      (:SRC . "cap.png") (:TITLE . "Ajusta")
-      (:TEXT . "Aqui ficar o text que serão o teste")))
+      (:SRC . "cap.png") (:TITLE . "A memorização é um problema.")
+      (:TEXT . "Utiliza-se do Memory e saiba como é possivel aprender varios conteúdos  sem esquecer nada.")))
     ((:ID . 2)
      (:ITEM (:CLASS-NAME . "d-block w-100")
       (:NOME . "Slider2") (:WIDTH . "100px")
-      (:SRC . "cap.png") (:TITLE . "title")
-      (:TEXT . "Testando Banckend Modification.")))) )
+      (:SRC . "acdemy-ia.png") (:TITLE . "Aprendizado e Computer IA")
+      (:TEXT . "Utiliza-se de Inteligência Artifical para lhe ajudar a aprender;")))) )
+
+
+(defparameter *info-top-default*
+  '(((:ID . 1)
+     (:PAG (:TITLE . "Exemplos de FlashCards:")
+      (:TEXT
+       . "O sistema a partir de um texto irá criar um sistema de cartas, aonde cartas possuirá um time de recordação, assim você utilizar-se do métodos MMBB (método de memorização na base binário), você não precisá se preocupa-se de quando o texto ou palavra teverá ser revisado.")) )) )
 
 
 
+(defparameter *info-down-default*
+  '(((:ID . 1)
+     (:PAG (:TITLE . "Que é MMBB?:")
+      (:TEXT
+       . "O MMBB é um método que ajusta a indentificar os intervalos de memórização, com base na na curva de esquecemento, aonde à partir do dia que lhe foi apresentado o conteúdo, já no dia seguinte estará esquencido 30% do que foi aprendido, então este método utiliza-se de um sistema número binário que irá definir quando você derá relembra o que foi aprendido, assim você força o aprendizado, chegado ao um resultado aonde o intervalo de reapredizado é tão simple e grande, que à partir de um momento nunca mais será nessário o reaprendizado.")))) )
+
+
+
+(defparameter *card-default*
+  '(((:ID . 1)
+  (:CARTAS (:ID . 1) (:WIDTH . "18rem") (:LENGHT . "18rem") (:LOCATION . "top")
+   (:IMG (:WIDTH . "20px") (:LENGTH . "20px") (:SRC . "apple.png"))
+   (:TEXT (:PRINCIPAL . "apple") (:INFO . "Yo como una manzana")
+	 (:CLASS-NAME . "list-group-flush"))
+   (:LANGUAGE ((:ID . 2) (:ORIGEM . "ES") (:WORD . "manzana"))))
+     (:LINK (:EDIT . "#editar") (:INFO . "#info")))
+    ((:ID . 2)
+     (:CARTAS (:ID . 1) (:WIDTH . "18rem") (:LENGHT . "18rem")
+      (:LOCATION . top)
+      (:IMG (:WIDTH . "20px") (:LENGTH . "20px") (:SRC . "apple.png"))
+      (:TEXT (:PRINCIPAL . "apple") (:INFO . "Eu como uma maçã.")
+	    (:CLASS-NAME . "list-group-flush"))
+      (:LANGUAGE (("ID" . 2) (:ORIGEM . "PT_BR") (:WORD . "maçã"))))
+     (:LINK (:EDIT . "#editar") (:INFO . "#info")))
+    ((:ID . 3)
+     (:CARTAS (:ID . 1) (:WIDTH . "18rem") (:LENGHT . "18rem")
+      (:LOCATION . top)
+      (:IMG (:WIDTH . "20px") (:LENGTH . "20px") (:SRC . "apple.png"))
+      (:TEXT (:PRINCIPAL . "apple") (:INFO ."私はりんごをたべります。")
+	    (:CLASS-NAME . "list-group-flush"))
+      (:LANGUAGE ((:ID . 3) (:ORIGEM . "JP") (:WORD . "りんご"))))
+     (:LINK (:EDIT . "#editar") (:INFO . "#info")))) )
+
+  
 (defun error-sys-html(c)
   (format t "Context-type: text/html~%~%")
   (format t "<html><body><h1>ERRO REQUEST: ~a</h1></body></html>" c))
@@ -133,6 +174,35 @@
   (setf (hunchentoot:header-out "Access-Control-Allow-Headers")
 	  "Content-Type, authorization")
   (format nil "~A" (json:encode-json-to-string *slide-default*)))
+
+
+(hunchentoot:define-easy-handler (info-top :uri "/system/infotop") ()
+  (setf (hunchentoot:content-type*) "application/json; charset=utf-8")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Origin") "*")
+  (setf (hunchentoot:header-out "Content-Type") "application/json")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Headers")
+	"Content-Type, authorization")
+  (format nil "~A" (json:encode-json-to-string *info-top-default*)))
+
+
+
+(hunchentoot:define-easy-handler (info-down :uri "/system/infodown") ()
+  (setf (hunchentoot:content-type*) "application/json; charset=utf-8")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Origin") "*")
+  (setf (hunchentoot:header-out "Content-Type") "application/json")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Headers")
+	"Content-Type, authorization")
+  (format nil "~A" (json:encode-json-to-string *info-down-default*)))
+
+
+(hunchentoot:define-easy-handler (info-card :uri "/system/card") ()
+  (setf (hunchentoot:content-type*) "application/json; charset=utf-8")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Origin") "*")
+  (setf (hunchentoot:header-out "Content-Type") "application/json")
+  (setf (hunchentoot:header-out "Access-Control-Allow-Headers")
+	"Content-Type, authorization")
+  (format nil "~A" (json:encode-json-to-string *card-default*)))
+
 
 
 (hunchentoot:define-easy-handler (auth :uri "/users/authenticate"
